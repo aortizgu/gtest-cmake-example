9a1f07cc0e6ff71640ea0f6f3726a256635f5041
Adrián Ortiz <ortiz.gutierrez.adrian@gmail.com>
2023-02-28 23:04:31 +0100

    fix gtest include

--- main/main.cpp
+++ main/main.cpp
@@ -1,8 +1,10 @@
 #include "bar.h"
 #include "foo.h"
+#include <iostream>
 
 int main(int argc, char *argv[])
 {
+    std::cout<<"Hola"<<std::endl;
     Bar bar;
     Foo foo(bar);
     foo.baz(true);
